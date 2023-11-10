4612d0c56ccf4911acfa63191284869422ed8e9f
xzxjesse <jessicaeveline121@gmail.com>
2023-11-09 21:47:07 -0300

    lista3

---
+++ lista3/enviados/problem_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct fila {
+int *dados;
+int N, p, u;
+} fila;
+
+int desenfileira (fila *f, int *y){
+    if (f->p == f->u) {
+        return 0;
+    }
+
+    *y = f->dados[f->p];
+    f->p = (f->p + 1);
+
+    return 1;
+}
